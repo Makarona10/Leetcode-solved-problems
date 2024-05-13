7d3f578b5658bc4007379a4be06bc8beeb380d13
Ahmed Abdelnaby <120122678+Makarona10@users.noreply.github.com>
2024-05-13 23:26:39 +0300

    Time: 62 ms (82.22%), Space: 56.9 MB (51.5%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.js
+++ 2634-minimum-common-value/2634-minimum-common-value.js
@@ -4,8 +4,8 @@
  * @return {number}
  */
 var getCommon = function(nums1, nums2) {
-    for (let x = 0; x < nums1.length; x++) {
-        for (let y = 0; y < nums2.length; y++) {
+    for (let x = 0, y = 0; x < nums1.length; x++) {
+        for (; y < nums2.length; y++) {
             if (nums1[x] === nums2[y])
                 return nums1[x];
             if (nums2[y] > nums1[x])
