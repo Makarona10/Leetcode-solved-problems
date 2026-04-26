1d03446e1f4264b5063f433048031c748c87f089
Ahmed Abdelnaby <120122678+Makarona10@users.noreply.github.com>
2026-04-26 06:02:20 +0300

    Implement integer to Roman conversion function

--- 0012-integer-to-roman/0012-integer-to-roman.ts
+++ 0012-integer-to-roman/0012-integer-to-roman.ts
@@ -48,3 +48,24 @@ function intToRoman(num: number): string {
 
   return result;
 }
+
+
+// Best Solution with O(1)
+const ROMAN_MAP: [number, string][] = [
+  [1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+  [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'],
+  [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
+];
+
+function intToRoman(num: number): string {
+  let result = "";
+  
+  for (const [val, char] of ROMAN_MAP) {
+    while (num >= val) {
+      result += char;
+      num -= val; 
+    }
+  }
+  
+  return result;
+}
