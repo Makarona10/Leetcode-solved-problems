e1470bc283e7a3924044e0b977ad378f4460f254
Ahmed Abdelnaby <120122678+Makarona10@users.noreply.github.com>
2026-04-27 02:04:42 +0300

    Time: 5 ms (35.79%), Space: 62.5 MB (39.51%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  const head = l1;
+  let prev: ListNode | null = null;
+  let remainder = 0;
+  while (l1) {
+    l1.val = l1.val + (l2?.val || 0) + remainder;
+    remainder = Math.floor(l1.val / 10)
+    l1.val = l1.val % 10;
+    prev = l1;
+    l1 = l1.next;
+    l2 = l2?.next || null;
+    if (!l1 && l2) {
+      prev.next = l2;
+      l1 = l2;
+      l2 = null;
+    }
+    if (remainder && !l1) {
+      l1 = new ListNode(remainder)
+      prev.next = l1;
+      remainder = 0;
+    }
+  }
+
+  return head;
+};
