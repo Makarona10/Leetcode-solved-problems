2de31dd8fd97f89cdc2aed13cb708aa242b8b111
Ahmed Abdelnaby <120122678+Makarona10@users.noreply.github.com>
2026-04-26 23:52:08 +0300

    Time: 2 ms (57.58%), Space: 58.3 MB (21.21%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.ts
+++ 1528-shuffle-string/1528-shuffle-string.ts
@@ -1,21 +1,9 @@
 function restoreString(s: string, indices: number[]): string {
-  if (s.length <= 1) return s;
-  const sArr = s.split("")
-  let current: number;
-  let tmp: number | string;
-  for (let idx = 1; idx < s.length; idx++) {
-    current = idx;
-    while (current >= 1) {
-      if (indices[current] < indices[current - 1]) {
-        tmp = indices[current - 1];
-        indices[current - 1] = indices[current];
-        indices[current]= tmp;
-        tmp = sArr[current - 1];
-        sArr[current - 1] = sArr[current];
-        sArr[current]= tmp;
-      }
-      current--;
-    }
-  };
-  return sArr.join("");
-};
+  const result = new Array(s.length);
+
+  for (let i = 0; i < s.length; i++) {
+    result[indices[i]] = s[i];
+  }
+
+  return result.join("");
+}
